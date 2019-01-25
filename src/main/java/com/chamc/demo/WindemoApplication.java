200a68549348b9b7f26242aa88c828f1bed1ac6c
MoonSmile <pkusser@sina.cn>
2019-01-25 09:05:59 +0800

    win10 通过maven plugin 进行docker build

---
+++ src/main/java/com/chamc/demo/WindemoApplication.java
@@ -0,0 +1,14 @@
+package com.chamc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindemoApplication.class, args);
+	}
+
+}
+
